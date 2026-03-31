e79370b9c287e68edc5eb4d39334ca89160affdc
pedromorais15 <pedro.morais.senai@gmail.com>
2026-03-31 10:34:54 -0300

    refatora app.py para melhorar a estrutura e mensagens de erro

--- app.py
+++ app.py
@@ -1,163 +1,168 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import random
 import firebase_admin
 from firebase_admin import credentials, firestore
-from auth import  token_obrigatorio, gerar_token
-from flask import CORS
-import os
+from auth import token_obrigatorio, gerar_token
 from dotenv import load_dotenv
+import os
 import json
 
 load_dotenv()
 
-
-
 app = Flask(__name__)
-
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
 CORS(app, origins="*")
-
-
 
 ADM_USUARIO = os.getenv("ADM_USUARIO")
 ADM_SENHA = os.getenv("ADM_SENHA")
 
-
+# =========================================
+#     FIREBASE / FIRESTORE
+# =========================================
 if os.getenv("VERCEL"):
-    #ONLINE NA VERCEL
-    cred = credentials.Certificate(json.loads(os.getenv("FIREBASE_CREDENTIALS")))
+    cred_json = os.getenv("FIREBASE_CREDENTIALS")
+
+    if not cred_json:
+        raise ValueError("FIREBASE_CREDENTIALS não encontrada")
+
+    cred = credentials.Certificate(json.loads(cred_json))
 else:
-    #LOCAL
     cred = credentials.Certificate("firebase.json")
-#Carregadr as credenciais do Firebase
 
-firebase_admin.initialize_app(cred)
+if not firebase_admin._apps:
+    firebase_admin.initialize_app(cred)
 
-# Cocnectar-se ao Firestore 
 db = firestore.client()
 
-
-
-#Rota principal de boas vindas
-@app.route("/", methods=['GET'])
+# =========================================
+#              ROTA PRINCIPAL
+# =========================================
+@app.route("/", methods=["GET"])
 def root():
     return jsonify({
-        "api": "charadas",
+        "api": "Charadas",
         "version": "1.0",
-        "Author": "Pedro"
+        "autor": "Joaquim"
     }), 200
 
 
-# ===============================
-#        ROTA DE LOGIN
-# ===============================
-@app.route("/login", methods=['POST'])
+# =========================================
+#               LOGIN
+# =========================================
+@app.route("/login", methods=["POST"])
 def login():
     dados = request.get_json()
 
     if not dados:
-        return jsonify({"error": "Envie os dados para login"}), 400
+        return jsonify({"error": "Envie os dados para login!"}), 400
 
     usuario = dados.get("usuario")
     senha = dados.get("senha")
 
     if not usuario or not senha:
-        return jsonify({"error": "Usuario e senha são obrigatorias"}), 400
-    
+        return jsonify({"error": "Usuário e senha são obrigatórios!"}), 400
+
     if usuario == ADM_USUARIO and senha == ADM_SENHA:
         token = gerar_token(usuario)
         return jsonify({
-            "message":"Login realizado com sucesso!",
+            "message": "Login realizado com sucesso!",
             "token": token
         }), 200
-    return jsonify({"error": "Usuario ou senha invalidos!"})
-    
-#Rota 1 - Método GET para retornar uma charada aleatória
-@app.route("/charadas", methods=['GET'])
-def get_charada():
-    charadas = [] #Lista vazia
-    lista = db.collection("charadas").stream() # Lista todos os daodos
 
-    #Transforma objetos do firestore em dicionário Python
+    return jsonify({"error": "Usuário ou senha inválidos"}), 401
+
+
+# =========================================
+#            GET - LISTAR TODAS
+# =========================================
+@app.route("/charadas", methods=["GET"])
+def get_charadas():
+    charadas = []
+    lista = db.collection("charadas").stream()
+
     for item in lista:
-        charadas.append(item.to_dict()) #Adiciona a charada à lista
+        charadas.append(item.to_dict())
 
     return jsonify(charadas), 200
 
-#Rota 2 - Método GET - Charadas aleatórias
-@app.route("/charadas/aleatoria", methods=['GET'])
-def get_charada_ramdom():
-    charadas = [] #Lista vazia
-    lista = db.collection("charadas").stream() # Lista todos os daodos
 
-    #Transforma objetos do firestore em dicionário Python
+# =========================================
+#          GET - ALEATÓRIA
+# =========================================
+@app.route("/charadas/aleatoria", methods=["GET"])
+def get_charada_random():
+    charadas = []
+    lista = db.collection("charadas").stream()
+
     for item in lista:
-        charadas.append(item.to_dict()) #Adiciona a charada à lista
+        charadas.append(item.to_dict())
 
     return jsonify(random.choice(charadas)), 200
 
 
-#Rota 3 - Método GET - Obter charada por ID
-@app.route("/charadas/<int:id>", methods=['GET'])
+# =========================================
+#          GET - POR ID
+# =========================================
+@app.route("/charadas/<int:id>", methods=["GET"])
 def get_charada_by_id(id):
-    lista = db.collection("charadas").where("id", "==", id).stream() #Busca a charada com o ID específico
-    
+    lista = db.collection("charadas").where("id", "==", id).stream()
+
     for item in lista:
         return jsonify(item.to_dict()), 200
-    
-    return jsonify({"error":"Charada não encontrada"}), 404
 
-# Rota 4 - Método POST - Criar nova charadas
-@app.route("/charadas", methods=['POST'])
+    return jsonify({"error": "Charada não encontrada"}), 404
+
+
+# =========================================
+#          POST - ADICIONAR
+# =========================================
+@app.route("/charadas", methods=["POST"])
 @token_obrigatorio
-def post_charadas():
-    
-    
+def post_charada():
     dados = request.get_json()
 
     if not dados or "pergunta" not in dados or "resposta" not in dados:
-        return jsonify({"error":"Dados inválidos ou incompletos!"}), 400
-    
+        return jsonify({"error": "Dados incompletos!"}), 400
+
     try:
-        # Busca pelo contador
         contador_ref = db.collection("contador").document("controle_id")
         contador_doc = contador_ref.get()
         ultimo_id = contador_doc.to_dict().get("ultimo_id")
-        # Somar 1 ao ultimo id
+
         novo_id = ultimo_id + 1
-        # Atualizar o id do contador
         contador_ref.update({"ultimo_id": novo_id})
 
-        # Cadastrar a nova charada
         db.collection("charadas").add({
             "id": novo_id,
             "pergunta": dados["pergunta"],
             "resposta": dados["resposta"]
         })
 
-        return jsonify({"message":"Charada criada com sucesso!",
-                        "id": id}), 201
-    except:
-        return jsonify({"error":"Falha no envio da charada"}), 400
+        return jsonify({"message": "Charada adicionada com sucesso!"}), 201
 
-# Rota 5 - Método PUT - Alteração total
-@app.route("/charadas/<int:id>", methods=['PUT'])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# =========================================
+#         PUT - ALTERAÇÃO TOTAL
+# =========================================
+@app.route("/charadas/<int:id>", methods=["PUT"])
 @token_obrigatorio
-def chararas_put(id):
-    
-    
+def charadas_put(id):
     dados = request.get_json()
 
-    # PUT - É necessário enviar PERGUNTA e RESPOSTA
     if not dados or "pergunta" not in dados or "resposta" not in dados:
-        return jsonify({"error":"Dados inválidos ou incompletos!"}), 400
-    
+        return jsonify({"error": "Dados incompletos!"}), 400
+
     try:
-        docs = db.collection("charadas").where("id","==",id).limit(1).get()
+        docs = db.collection("charadas").where("id", "==", id).limit(1).get()
+
         if not docs:
-            return jsonify({"error":"Charada não encontrada"}), 404
-        
-        # Pega o primeiro (e único) documento da lista
+            return jsonify({"error": "Charada não encontrada!"}), 404
+
         for doc in docs:
             doc_ref = db.collection("charadas").document(doc.id)
             doc_ref.update({
@@ -165,68 +170,75 @@ def chararas_put(id):
                 "resposta": dados["resposta"]
             })
 
-        return jsonify({"message": "Charada alterada com sucesso"}), 200
-    except:
-        return jsonify({"error":"Falha na alteração da charada"}), 400
+        return jsonify({"message": "Charada alterada com sucesso!"}), 200
 
- 
-# Rota 6 - Método PATCH - Alteração parcial
-@app.route("/charadas/<int:id>", methods=['PATCH'])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# =========================================
+#       PATCH - ALTERAÇÃO PARCIAL
+# =========================================
+@app.route("/charadas/<int:id>", methods=["PATCH"])
 @token_obrigatorio
-def chararas_patch(id):
-    
+def charadas_patch(id):
     dados = request.get_json()
 
-    # PATCH - pode alterar só pergunta ou só resposta
     if not dados or ("pergunta" not in dados and "resposta" not in dados):
-        return jsonify({"error":"Dados inválidos!"}), 400
-    
+        return jsonify({"error": "Dados incompletos!"}), 400
+
     try:
-        docs = db.collection("charadas").where("id","==",id).limit(1).get()
+        docs = db.collection("charadas").where("id", "==", id).limit(1).get()
+
         if not docs:
-            return jsonify({"error":"Charada não encontrada"}), 404
-        
+            return jsonify({"error": "Charada não encontrada!"}), 404
+
         doc_ref = db.collection("charadas").document(docs[0].id)
-        update_charada = {}
+
+        update_charadas = {}
+
         if "pergunta" in dados:
-            update_charada["pergunta"] = dados["pergunta"]
+            update_charadas["pergunta"] = dados["pergunta"]
 
         if "resposta" in dados:
-            update_charada["resposta"] = dados["resposta"]
+            update_charadas["resposta"] = dados["resposta"]
 
-        # Atualiza o Firestore
-        doc_ref.update(update_charada)
+        doc_ref.update(update_charadas)
 
-        return jsonify({"message": "Charada alterada com sucesso"}), 200
+        return jsonify({"message": "Charada alterada com sucesso!"}), 200
 
-    except:
-        return jsonify({"error":"Falha na alteração da charada"}), 400
-    
-# Rota 7 - DELETE - Excluir charada
-@app.route("/charadas/<int:id>", methods=['DELETE'])
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+
+# =========================================
+#             DELETE
+# =========================================
+@app.route("/charadas/<int:id>", methods=["DELETE"])
 @token_obrigatorio
-def delete_charada(id):
-    
-
-    docs = db.collection("charadas").where("id","==",id).limit(1).get()
+def charadas_delete(id):
+    docs = db.collection("charadas").where("id", "==", id).limit(1).get()
 
     if not docs:
-        return jsonify({"error":"Charada não encontrada"}), 404
+        return jsonify({"message": "Charada não encontrada!"}), 404
 
     doc_ref = db.collection("charadas").document(docs[0].id)
     doc_ref.delete()
-    return jsonify({"message":"Charada excluída com sucesso!"}), 200
 
-# ====================
-#  Rotas de tratamento de erros
-# ====================
+    return jsonify({"message": "Charada excluída com sucesso!"}), 200
+
+
+# =========================================
+#        TRATAMENTO DE ERROS
+# =========================================
 @app.errorhandler(404)
-def erro404(error):
-    return jsonify({"error":"URL não encontrada"}), 404
+def not_found(error):
+    return jsonify({"error": "Página não encontrada!"}), 404
+
 
 @app.errorhandler(500)
-def erro500(error):
-    return jsonify({"error":"Servidor interno com falhas. Tente mais tarde"}), 500
+def internal_error(error):
+    return jsonify({"error": "Erro interno do servidor!"}), 500
 
 
 if __name__ == "__main__":
